42105243eb32edb8995e0782769937ea189a1f0d
Sean O'Daniels <sodaniels@gmail.com>
2024-08-22 09:17:49 -0400

    Refactored data.

--- courseApp/src/App.tsx
+++ courseApp/src/App.tsx
@@ -1,23 +1,9 @@
 import Header from './components/Header'
 import Content from './components/Content'
 import Total from './components/Total'
+import { courseName, courseParts } from './data/courses.ts'
 
 const App = () => {
-  const courseName = 'Half Stack application development'
-  const courseParts = [
-    {
-      name: 'Fundamentals',
-      exerciseCount: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exerciseCount: 7
-    },
-    {
-      name: 'Deeper type usage',
-      exerciseCount: 14
-    }
-  ]
 
   const totalExercises = courseParts.reduce((sum, part) => sum + part.exerciseCount, 0)
 
