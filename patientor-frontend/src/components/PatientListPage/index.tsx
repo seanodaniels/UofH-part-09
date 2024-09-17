9db965b521484708f86875e991fabeb2180659ee
Sean O'Daniels <sodaniels@gmail.com>
2024-09-17 12:28:40 -0400

    removed placeholder link from frontend

--- patientor-frontend/src/components/PatientListPage/index.tsx
+++ patientor-frontend/src/components/PatientListPage/index.tsx
@@ -50,11 +50,6 @@ const PatientListPage = ({ patients, setPatients } : Props ) => {
 
   return (
     <div className="App">
-      <Box>
-        <Typography align="center" variant="h6">
-          Patient list <Link to="https://dkfskldjkl.com">this is a random link</Link>
-        </Typography>
-      </Box>
       <Table style={{ marginBottom: "1em" }}>
         <TableHead>
           <TableRow>
